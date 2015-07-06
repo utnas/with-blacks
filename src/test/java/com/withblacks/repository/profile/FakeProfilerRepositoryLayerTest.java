0c68292392599e1ffd20642f917cd257b05aec5c
dnsewolo <dnsewolo@teksystems.com>
2015-07-06 16:56:31 -0400

    refactoring of dependencies

--- src/test/java/com/withblacks/repository/profile/FakeProfilerRepositoryLayerTest.java
+++ src/test/java/com/withblacks/repository/profile/FakeProfilerRepositoryLayerTest.java
@@ -1,30 +1,35 @@
 package com.withblacks.repository.profile;
 
+import com.withblacks.business.builder.ProfilerBuilderTest;
+import com.withblacks.business.entity.Profiler;
 import org.junit.Before;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.*;
 
 public class FakeProfilerRepositoryLayerTest {
 
+    FakeProfilerRepositoryLayer repositoryLayer;
+
     @Before
     public void setUp() throws Exception {
-
+        repositoryLayer = new FakeProfilerRepositoryLayer();
     }
 
     @Test
     public void testFind() throws Exception {
-
+        assertNotNull(repositoryLayer.find(1L));
     }
 
     @Test
     public void testFindAll() throws Exception {
-
+        assertThat(repositoryLayer.findAll().size(), is(1));
     }
 
     @Test
     public void testFind1() throws Exception {
-
+        assertNotNull(repositoryLayer.find("FirstName"));
     }
 
     @Test
