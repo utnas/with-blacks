05140be844fc7abf7b2575f8c332dd8410041752
Dooley <nsewolo@gmail.com>
2015-08-03 23:06:49 -0400

    UserDto tests added

--- src/main/java/com/withblacks/rest/user/UserDto.java
+++ src/main/java/com/withblacks/rest/user/UserDto.java
@@ -39,10 +39,6 @@ public class UserDto extends ResourceSupport {
         this.gender = gender;
     }
 
-    public Long getIds() {
-        return id;
-    }
-
     public void setId(long id) {
         this.id = id;
     }
--- src/test/java/com/withblacks/rest/user/UserDtoTest.java
+++ src/test/java/com/withblacks/rest/user/UserDtoTest.java
@@ -1,7 +1,11 @@
 package com.withblacks.rest.user;
 
+import com.withblacks.business.entities.GENDER;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class UserDtoTest {
 
@@ -9,40 +13,43 @@ public class UserDtoTest {
 
     @Before
     public void setUp() throws Exception {
+        dto = new UserDtoBuilder()
+                .setFirstName("Iron")
+                .setLastName("Man")
+                .setGender(GENDER.MALE)
+                .build();
     }
 
     @Test
-    public void testGetFirstName() throws Exception {
-
+    public void itShouldGetFirstName() throws Exception {
+        assertThat(dto.getFirstName(), is("Iron"));
     }
 
     @Test
-    public void testSetFirstName() throws Exception {
-
+    public void itShouldSetFirstName() throws Exception {
+        dto.setFirstName("Super");
+        assertThat(dto.getFirstName(), is("Super"));
     }
 
     @Test
-    public void testGetLastName() throws Exception {
-
+    public void itShouldGetLastName() throws Exception {
+        assertThat(dto.getLastName(), is("Man"));
     }
 
     @Test
-    public void testSetLastName() throws Exception {
-
+    public void itShouldSetLastName() throws Exception {
+        dto.setLastName("Woman");
+        assertThat(dto.getLastName(), is("Woman"));
     }
 
     @Test
     public void testGetGender() throws Exception {
-
+        assertThat(dto.getGender(), is(GENDER.MALE));
     }
 
     @Test
-    public void testSetGender() throws Exception {
-
-    }
-
-    @Test
-    public void testGetIds() throws Exception {
-
+    public void itShouldSetGender() throws Exception {
+        dto.setGender(GENDER.FEMALE);
+        assertThat(dto.getGender(), is(GENDER.FEMALE));
     }
 }
