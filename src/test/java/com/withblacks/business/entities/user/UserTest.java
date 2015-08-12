262d6dd5d1b994fc3a915fa0dcd378261028a35a
Dooley <nsewolo@gmail.com>
2015-08-11 23:32:27 -0400

    Renaming test data

--- src/test/java/com/withblacks/business/entities/user/UserTest.java
+++ src/test/java/com/withblacks/business/entities/user/UserTest.java
@@ -1,12 +1,9 @@
 package com.withblacks.business.entities.user;
 
-import com.withblacks.business.entities.user.GENDER;
-import com.withblacks.business.entities.user.User;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.math.BigDecimal;
-
+import static com.withblacks.business.entities.user.GENDER.FEMALE;
 import static com.withblacks.business.entities.user.UserBuilder.build;
 import static org.hamcrest.core.Is.is;
 import static org.springframework.test.util.MatcherAssertionErrors.assertThat;
@@ -17,22 +14,22 @@ public class UserTest {
 
     @Before
     public void setUp() throws Exception {
-        user = build("FirstName", "LastName", GENDER.FEMALE);
+        user = build("Super", "Man", FEMALE);
     }
 
     @Test
     public void testGetFirstName() throws Exception {
-        assertThat(user.getFirstName(), is("FirstName"));
+        assertThat(user.getFirstName(), is("Super"));
     }
 
     @Test
     public void testGetLastName() throws Exception {
-        assertThat(user.getLastName(), is("LastName"));
+        assertThat(user.getLastName(), is("Man"));
     }
 
     @Test
     public void testGetGender() throws Exception {
-        assertThat(user.getGender().toString(), is("FEMALE"));
+        assertThat(user.getGender(), is(FEMALE));
     }
 
     @Test
