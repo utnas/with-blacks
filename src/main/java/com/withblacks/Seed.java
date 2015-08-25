26de60555a66fe4a897f47e1294333be007ae318
dnsewolo <dnsewolo@teksystems.com>
2015-08-25 13:09:49 -0400

    SQLite dependency added Extracted the Seed class to populate Repository

---
+++ src/main/java/com/withblacks/Seed.java
@@ -0,0 +1,51 @@
+package com.withblacks;
+
+import com.withblacks.business.entities.project.ProjectBuilder;
+import com.withblacks.business.entities.user.UserBuilder;
+import com.withblacks.repository.project.ProjectRepository;
+import com.withblacks.repository.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static com.withblacks.business.entities.user.GENDER.FEMALE;
+import static com.withblacks.business.entities.user.GENDER.MALE;
+
+public class Seed {
+    private  UserRepository userRepository;
+    private  ProjectRepository projectRepository;
+
+    public Seed(){}
+
+    @Autowired
+    public Seed(final UserRepository userRepository, final ProjectRepository projectRepository) {
+        this.userRepository = userRepository;
+        this.projectRepository = projectRepository;
+    }
+
+    public void seed() {
+        userRepository.save(new UserBuilder()
+                .setFirstName("Iron")
+                .setLastName("Man")
+                .setGender(MALE).build());
+
+        userRepository.save(new UserBuilder()
+                .setFirstName("Super")
+                .setLastName("Man")
+                .setGender(MALE).build());
+
+        userRepository.save(new UserBuilder()
+                .setFirstName("Cat")
+                .setLastName("Woman")
+                .setGender(FEMALE).build());
+
+        userRepository.save(new UserBuilder()
+                .setFirstName("Spider")
+                .setLastName("Man")
+                .setGender(MALE).build());
+
+        projectRepository.save(new ProjectBuilder()
+                .setName("SPM-st").build());
+
+        projectRepository.save(new ProjectBuilder()
+                .setName("Arm-st").build());
+    }
+}
