d4e433bcb5442029a57aafbf118f30ea7033e6a5
dnsewolo <dnsewolo@teksystems.com>
2015-08-27 10:28:28 -0400

    Transformation to DTO

--- src/main/java/com/withblacks/facade/project/dto/ProjectMapper.java
+++ src/main/java/com/withblacks/facade/project/dto/ProjectMapper.java
@@ -1,5 +1,7 @@
 package com.withblacks.facade.project.dto;
 
+import com.google.common.base.Function;
+import com.google.common.collect.Iterables;
 import com.withblacks.business.entities.project.ProjectBuilder;
 import com.withblacks.business.entities.project.Project;
 import com.withblacks.facade.project.dto.ProjectDto;
@@ -7,9 +9,13 @@ import com.withblacks.facade.project.dto.ProjectDtoBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.List;
 
+import static com.google.common.collect.Iterables.transform;
 import static com.google.common.collect.Lists.newArrayList;
+import static java.util.Collections.EMPTY_LIST;
 
 @Component
 public class ProjectMapper {
@@ -24,10 +30,14 @@ public class ProjectMapper {
     }
 
     public Iterable<ProjectDto> convertToDtos(final Iterable<Project> projects) {
-        final List<ProjectDto> result = newArrayList();
-        for (final Project project : projects) {
-            result.add(convertToDto(project));
+        try {
+            return transform(projects, toDto());
+        } catch (IndexOutOfBoundsException e) {
+            return EMPTY_LIST;
         }
-        return result;
+    }
+
+    private Function<Project, ProjectDto> toDto() {
+        return (project) -> convertToDto(project);
     }
 }
