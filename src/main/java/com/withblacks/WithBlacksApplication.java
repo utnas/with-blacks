a6a3b16802e918fcb7172ae48542e8036a100550
dnsewolo <nsewolo@gmail.com>
2015-10-02 09:46:37 -0400

    documentation about a way to get token

--- src/main/java/com/withblacks/WithBlacksApplication.java
+++ src/main/java/com/withblacks/WithBlacksApplication.java
@@ -1,5 +1,29 @@
 package com.withblacks;
 
+/**
+ * After you launch the app, you can seek a bearer token like this:
+ * <p>
+ * <pre>
+ * curl localhost:8080/oauth/token -d "grant_type=password&scope=write&username=user&password=pwd" -u user:pwd
+ * </pre>
+ * <ul>
+ * <li>grant_type=password (user credentials will be supplied)</li>
+ * <li>scope=read (or write | trusted)</li>
+ * <li>username=user (username checked against user details service)</li>
+ * <li>password=pwd (password checked against user details service)</li>
+ * <li>-u user:pwd (clientid:user)</li>
+ * </ul>
+ * <p>
+ * Response should be similar to this:
+ * <code>{"access_token":"533de99b-5a0f-4175-8afd-1a64feb952d5","token_type":"bearer","expires_in":43199,"scope":"read"}</code>
+ * <p>
+ * With the token value, you can now interrogate the RESTful interface like this:
+ * <p>
+ * <pre>
+ * curl -H "Authorization: bearer [access_token]" localhost:8080/users
+ * </pre>
+ */
+
 import com.withblacks.business.seeds.Seed;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
